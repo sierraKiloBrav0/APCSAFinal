061e8ccf9bda16fd2b36c85a75ce59c5d9bd8056
sabrinab <150946276+sierraKiloBrav0@users.noreply.github.com>
2025-01-06 17:11:11 +0000

    new file:   fileReader.java     debugger says it contains errors but that's     because i haven't made a runner class yet

---
+++ fileReader.java
@@ -0,0 +1,25 @@
+import java.util.*;  
+import java.io.*; 
+
+class fileReader { 
+    ArrayList<String> lines; 
+    public int currentIndex; 
+    private String filePath; 
+
+    public fileReader(String filePath){ 
+        this.filePath = filePath; // store the file path 
+
+        lines = new ArrayList<>(); 
+        currentIndex = 0; 
+
+        try (Scanner in = new Scanner(new File (filePath))){ 
+            // read the file 
+            while (in.hasNextLine()){ 
+                // add lines to ArrayList 
+                String line = in.nextLine(); 
+                lines.add(line); 
+                System.out.println("Line added: " + line);
+            }
+        }
+    }
+}
