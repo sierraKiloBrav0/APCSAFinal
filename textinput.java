b3c96aa1bfe44e6f6943cf0568829cc3fbf76ee3
RoryM0 <rory5827@outlook.com>
2025-01-07 08:42:36 -0600

    Update textinput.java

--- textinput.java
+++ textinput.java
@@ -1,5 +1,14 @@
 public class textinput {
     public static void main(String[]args){
+
+    }
+    //this will contain the main loop that will prompt the user for input and return 
+    public void userloop(){
         
     }
+    //
+    public void splitInput(){
+
+    }
+
 }
