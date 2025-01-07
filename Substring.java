d070f422c406e93ea9ff6312d464bf9af674ef00
tgokhale7 <tejas.gokhale1@gmail.com>
2025-01-07 15:42:44 +0000

    Changed array to arrayList

--- Substring.java
+++ Substring.java
@@ -1,11 +1,11 @@
 public class Substring {
-    private String[] friendshipPosValues;
-    private String[] friendshipNegValues;
-    friendshipPosValues = FileReader.toStringArray("positive.txt");
-    friendshipNegValues = FileReader.toStringArray("negative.txt");
+    private ArrayList<String> friendshipPosValues;
+    private ArrayList<String> friendshipNegValues;
+    friendshipPosValues = FileReader.toStringList("positive.txt");
+    friendshipNegValues = FileReader.toStringList("negative.txt");
 
     public double getFriendshipValue(String userInput) {
-        for (int i = 0; i < friendshipPosValues.length; i++) {
+        for (int i = 0; i < friendshipPosValues.size(); i++) {
             String currentLine = friendshipPosValues.get(i);
             int location = currentLine.indexOf(",");
             String sub = currentLine.substring(0, location);
@@ -13,7 +13,7 @@ public class Substring {
                 return 1.0;
             }
             else{
-                for (int j = 0; j < friendshipNegValues.length; j++) {
+                for (int j = 0; j < friendshipNegValues.size(); j++) {
                     String currentLine2 = friendshipNegValues.get(j);
                     int location2 = currentLine2.indexOf(",");
                     String sub2 = currentLine2.substring(0, location2);
