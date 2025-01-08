c361bc6d8155f91346bac2c29f40a2eb6751d1b5
sabrinab <150946276+sierraKiloBrav0@users.noreply.github.com>
2025-01-08 08:17:18 -0600

    Update Substring.java

--- Substring.java
+++ Substring.java
@@ -7,17 +7,13 @@ public class Substring {
     public double getFriendshipValue(String userInput) {
         for (int i = 0; i < friendshipPosValues.size(); i++) {
             String currentLine = friendshipPosValues.get(i);
-            int location = currentLine.indexOf("-");
-            String sub = currentLine.substring(0, location);
-            if (userInput.contains(sub)) {
+            if (userInput.contains(currentLine)) {
                 return 1.0;
             }
             else{
                 for (int j = 0; j < friendshipNegValues.size(); j++) {
                     String currentLine2 = friendshipNegValues.get(j);
-                    int location2 = currentLine2.indexOf("-");
-                    String sub2 = currentLine2.substring(0, location2);
-                    if (userInput.contains(sub2)) {
+                    if (userInput.contains(currentLine2)) {
                         return -1.0;
                     }
 
