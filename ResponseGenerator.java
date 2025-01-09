6c56f228ec33ebb2c55ee78632eb624cbeb92428
sabrinab <150946276+sierraKiloBrav0@users.noreply.github.com>
2025-01-09 08:27:30 -0600

    Create ResponseGenerator.java

---
+++ ResponseGenerator.java
@@ -0,0 +1,30 @@
+public class ResponseGenerator { 
+    // variable declaration 
+    private String[] positiveResponses; // list of all positive responses to randomly select from 
+    private String[] negativeResponses; // list of all negative responses to randomly select from 
+    private double personalityValue = -20.0; // intial personality value 
+    private double sentenceValue; // sentimental value of the input to be reset at the start of each loop 
+
+    // call a positive response 
+    public String getPositiveResponse() { 
+        // filler
+    }
+
+    // call a negative response 
+    public String getNegativeResponse() { 
+        // filler 
+    }
+
+    //call a neutral response 
+
+    // logic to choose whether to give a positive, negative, or neutral response 
+    if ((personalityValue > -25.0 ) && (personalityValue < -10.0)){ 
+        // call getNegativeResponse
+    } else if ((personalityValue > -10.0) && (personalityValue < 10.0)) { 
+        // call getNeutralResponse
+    } else if (personalityValue > 10.0) { 
+        // call getPositiveResponse
+    } else { 
+        System.out.println("I will never talk to you again, you insignificant wretch! Get out of my sight!"); 
+    }
+}
