3cfb9aeaec6576fb23876e518d0efdcca07f9e92
sabrinab <150946276+sierraKiloBrav0@users.noreply.github.com>
2025-01-09 08:41:17 -0600

    Update FileReader.java

--- FileReader.java
+++ FileReader.java
@@ -1,25 +1,23 @@
-import java.util.*;  
-import java.io.*; 
-
 class FileReader { 
     ArrayList<String> lines; 
     public int currentIndex; 
     private String filePath; 
 
-    public fileReader(String filePath){ 
+    public ArrayList<String> getContent(String filePath){ 
         this.filePath = filePath; // store the file path 
 
         lines = new ArrayList<>(); 
         currentIndex = 0; 
 
-        try (Scanner in = new Scanner(new File (filePath))){ 
+        try (Scanner in = new Scanner(new File (filePath))) { 
             // read the file 
             while (in.hasNextLine()){ 
                 // add lines to ArrayList 
                 String line = in.nextLine(); 
                 lines.add(line); 
-                System.out.println("Line added: " + line);
-            }
+                currentIndex += 1; 
+            }  
         }
+        return lines; 
     }
 }
