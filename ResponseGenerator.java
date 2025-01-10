310344459e2feb5e7b02bf78e74273d285f9dff3
sabrinab <150946276+sierraKiloBrav0@users.noreply.github.com>
2025-01-10 08:37:24 -0600

    Update ResponseGenerator.java

--- ResponseGenerator.java
+++ ResponseGenerator.java
@@ -2,9 +2,9 @@ public class ResponseGenerator {
     // construct a file reader 
     FileReader in = new FileReader(); 
     // variable declaration 
-    private String[] positiveResponses = in.getContent(positiveResponses.txt); // list of all positive responses to randomly select from 
-    private String[] negativeResponses = in.getContent(negativeResponses.txt); // list of all negative responses to randomly select from 
-    private String[] neutralResponses = in.getContent(neutralResponses.txt); // list of all neutral responses to randomly select from
+    private String[] positiveResponses = in.getContent(main\\positiveResponses.txt); // list of all positive responses to randomly select from 
+    private String[] negativeResponses = in.getContent(main\\negativeResponses.txt); // list of all negative responses to randomly select from 
+    private String[] neutralResponses = in.getContent(main\\neutralResponses.txt); // list of all neutral responses to randomly select from
     private double personalityValue = -20.0; // intial personality value 
     private double sentenceValue = 0.0; // sentimental value of the input to be reset at the start of each loop 
     private int linePulled = 0; // random index to take a line from in a text file based on the size of the file 
